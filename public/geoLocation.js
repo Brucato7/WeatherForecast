4d691381ece8f2cc0591c902512b60619f543a08
Jonathon Weisse <jonathonweisse@gmail.com>
2016-07-07 15:14:41 -0500

    added node modules and postgresql db

---
+++ public/geoLocation.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+// 	$.getJSON("http://ip-api.com/json/", function(data){
+// 		console.log(data);
+// 	});
+$.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=New+York,+NY&key=AIzaSyDhaReQhYmwgJkA32hSAFOcXZ7k5dL1qSY", function(data){
+		console.log(data);
+	});
+
+	// This example displays an address form, using the autocomplete feature
+      // of the Google Places API to help users fill in the information.
+
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      // var placeSearch, autocomplete;
+      // // var componentForm = {
+      // //   street_number: 'short_name',
+      // //   route: 'long_name',
+      // //   locality: 'long_name',
+      // //   administrative_area_level_1: 'short_name',
+      // //   country: 'long_name',
+      // //   postal_code: 'short_name'
+      // // };
+
+      // function initAutocomplete() {
+      //   // Create the autocomplete object, restricting the search to geographical
+      //   // location types.
+      //   autocomplete = new google.maps.places.Autocomplete(
+      //       /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+      //       {types: ['geocode']});
+
+      //   // When the user selects an address from the dropdown, populate the address
+      //   // fields in the form.
+      //   //autocomplete.addListener('place_changed', fillInAddress);
+      // }
+
+      // // function fillInAddress() {
+      // //   // Get the place details from the autocomplete object.
+      // //   var place = autocomplete.getPlace();
+
+      // //   for (var component in componentForm) {
+      // //     document.getElementById(component).value = '';
+      // //     document.getElementById(component).disabled = false;
+      // //   }
+
+      // //   // Get each component of the address from the place details
+      // //   // and fill the corresponding field on the form.
+      // //   for (var i = 0; i < place.address_components.length; i++) {
+      // //     var addressType = place.address_components[i].types[0];
+      // //     if (componentForm[addressType]) {
+      // //       var val = place.address_components[i][componentForm[addressType]];
+      // //       document.getElementById(addressType).value = val;
+      // //     }
+      // //   }
+      // // }
+
+      // // Bias the autocomplete object to the user's geographical location,
+      // // as supplied by the browser's 'navigator.geolocation' object.
+      // function geolocate() {
+      //   if (navigator.geolocation) {
+      //     navigator.geolocation.getCurrentPosition(function(position) {
+      //       var geolocation = {
+      //         lat: position.coords.latitude,
+      //         lng: position.coords.longitude
+      //       };
+      //       var circle = new google.maps.Circle({
+      //         center: geolocation,
+      //         radius: position.coords.accuracy
+      //       });
+      //       autocomplete.setBounds(circle.getBounds());
+      //     });
+      //   }
+      // }
+
+	google.maps.event.addDomListener(window, 'load', intilize);
+	function intilize() {
+	    var autocomplete = new google.maps.places.Autocomplete(document.getElementById("autocomplete"));
+
+	    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+
+		    var place = autocomplete.getPlace();
+		    console.log(place);
+		    var lat = place.geometry.viewport.f.b;
+		    var long = place.geometry.viewport.b.b;
+		  	console.log(lat);
+		  	console.log(long);
+		    });
+
+	};
+
+
+	$("#typeItIn").append("<form role='form'>" +
+  						"<div class='form-group'>" +
+  						"<label for='autocomplete'>Enter your Address:</label><br>" +
+  						"<input id='autocomplete' placeholder='Enter your address' type='text'></input>"+
+  						"</div></form>");
+});
--- public/weather.js
+++ public/weather.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+var getWeather = function(){
   $.getJSON('https://api.forecast.io/forecast/3d4e42eb142384fe31939a4b9b8c14df/43.088481,-88.03204', function(data){
-  	var weather = data.daily.data.temperatureMax;
+  	console.log(data);
+
   });
-});
+}
+// });
 
 var fakeHighTemps = [62,83,99,77,12];
